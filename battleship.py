2d5bb0bc6a9e9f4b73d0b0bc0c176f8402018add
Alexandr Kuzmin <shurik.kuzmin@gmail.com>
2023-12-16 10:23:56 -0500

    Adding the drawing.

--- battleship.py
+++ battleship.py
@@ -3,22 +3,6 @@ import pygame.draw
 
 pygame.init()
 
-screen_width = 600
-screen_height = 600
-screen = pygame.display.set_mode((screen_width, screen_height))
-
-
-isRunning = True
-while isRunning:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            isRunning = False
-
-    pygame.draw.circle(screen,(255,0,0),(100,100), 20.0)
-    pygame.display.update()
-
-pygame.quit()
-
 field = [[0,0,0,0,1,0,0,0,1,0],
          [0,0,1,0,0,0,0,0,0,0],
          [0,0,0,0,0,1,0,1,0,1],
@@ -29,6 +13,33 @@ field = [[0,0,0,0,1,0,0,0,1,0],
          [0,0,0,0,0,0,0,0,0,0],
          [0,1,1,0,0,1,1,0,1,0],
          [0,0,0,0,0,0,0,0,1,0]]
+
+box_size = 60
+screen_width = box_size * 10
+screen_height = box_size * 10
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+def draw(field):
+    for row in range(10):
+        for column in range(10):
+            if field[row][column] == 1:
+                pygame.draw.rect(screen, (0,0,255),
+                ((column*box_size, row*box_size),(box_size, box_size)))
+            print(field[row][column])
+
+draw(field)
+
+isRunning = True
+while isRunning:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            isRunning = False
+
+    pygame.display.update()
+
+pygame.quit()
+
+
 
 print(field[3])
 
