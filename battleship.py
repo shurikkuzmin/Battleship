5e02e6f4b0473e49fc84553455543a65168b68f8
Alexandr Kuzmin <shurik.kuzmin@gmail.com>
2024-01-06 10:08:03 -0500

    Added the second field.

--- battleship.py
+++ battleship.py
@@ -14,21 +14,39 @@ field = [[0,0,0,0,1,0,0,0,1,0],
          [0,1,1,0,0,1,1,0,1,0],
          [0,0,0,0,0,0,0,0,1,0]]
 
+field_enemy = [[0,0,1,0,0,0,0,0,1,1],
+               [0,0,0,0,0,0,0,0,0,0],
+               [1,1,0,1,0,0,0,0,0,0],
+               [0,0,0,1,0,0,0,1,0,0],
+               [0,0,0,1,0,0,0,1,0,0],
+               [0,0,0,0,0,0,0,0,0,0],
+               [0,1,1,1,0,1,1,1,1,0],
+               [0,0,0,0,0,0,0,0,0,0],
+               [0,1,0,0,0,0,0,0,0,0],
+               [0,0,0,1,0,0,1,0,0,0]]
+
 box_size = 60
-screen_width = box_size * 10
+screen_width = box_size * 22
 screen_height = box_size * 10
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-def draw(field):
+def draw(field, isEnemy):
+    shift = 0
+    if isEnemy:
+        shift = 12 * box_size
+
     for row in range(10):
         for column in range(10):
             if field[row][column] == 1:
                 pygame.draw.rect(screen, (0,0,255),
-                ((column*box_size, row*box_size),(box_size, box_size)))
+                ((shift + column*box_size, row*box_size),(box_size, box_size)))
             pygame.draw.rect(screen, (255,255,255),
-            ((column*box_size, row*box_size),(box_size, box_size)),1)
+            ((shift + column*box_size, row*box_size),(box_size, box_size)),1)
 
-draw(field)
+
+
+draw(field, False)
+draw(field_enemy, True)
 
 isRunning = True
 while isRunning:
